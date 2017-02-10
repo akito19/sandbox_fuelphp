7f8f1160ec00159f47b318d0a849b4c2ad769412
SideCI <support@sideci.com>
2017-02-10 08:52:49 +0000

    Applied fixes from SideCI

--- fuel/app/config/routes.php
+++ fuel/app/config/routes.php
@@ -1,8 +1,8 @@
 <?php
 return array(
-	'_root_'  => 'welcome/index',  // The default route
-	'_404_'   => 'welcome/404',    // The main 404 route
-	
-	'hello(/:name)?' => array('welcome/hello', 'name' => 'hello'),
+    '_root_'  => 'welcome/index',  // The default route
+    '_404_'   => 'welcome/404',    // The main 404 route
+
+    'hello(/:name)?' => array('welcome/hello', 'name' => 'hello'),
   'profile(/:name)?' => array('welcome/profile', 'name' => 'profile')
 );
